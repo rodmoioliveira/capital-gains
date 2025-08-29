dfe3b616beab5dd20a5eb87001ad667b8933102f
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2025-08-29 20:18:36 -0300

    fix: Simplify doc-other-projects script
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- dev/doc-other-projects.sh
+++ dev/doc-other-projects.sh
@@ -12,13 +12,10 @@ deps-validate() {
   deps=(
     "cat"
     "gh"
+    "grep"
     "jq"
-    "rm"
     "sed"
-    "seq"
-    "sleep"
     "sort"
-    "sponge"
     "tee"
   )
   for dep in "${deps[@]}"; do
@@ -38,25 +35,17 @@ deps-validate() {
 }
 
 other-projects() {
-  if [[ -f OTHER_PROJECTS.md ]]; then
-    rm OTHER_PROJECTS.md
-  fi
-
-  local steps=5
-  for i in $(seq 1 "${steps}"); do
-    printf 1>&2 "Step %s/%s\n" "${i}" "${steps}"
-    gh api \
-      -H "Accept: application/vnd.github+json" \
-      -H "X-GitHub-Api-Version: 2022-11-28" \
-      '/search/code?page='"${i}"'&per_page=1000&q=+{"operation"%3A"sell"%2C+"unit-cost"%3A20.00%2C+"quantity"%3A+5000}' |
-      jq '.items[].repository.full_name' -r |
-      sort -u |
-      sed -E 's@(.+)@- [\1](https://github.com/\1)@g' |
-      tee -a OTHER_PROJECTS.md
-    sleep 5
-  done
-
-  cat OTHER_PROJECTS.md | sort -u | grep rodmoioliveira -v | sponge OTHER_PROJECTS.md
+  gh api \
+    --paginate \
+    --slurp \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    '/search/code?q=+{"operation"%3A"sell"%2C+"unit-cost"%3A20.00%2C+"quantity"%3A+5000}' |
+    jq '.[].items[].repository.full_name' -r |
+    sed -E 's@(.+)@- [\1](https://github.com/\1)@g' |
+    sort -u |
+    grep rodmoioliveira -v |
+    tee OTHER_PROJECTS.md
 }
 
 main() {
